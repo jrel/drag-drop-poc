df212af7ff37e67dd3a48aed69f494496a2754b3
João Lourenço <jrelourenco@outlook.pt>
2022-05-24 13:48:20 +0100

    wip

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkVirtualScrollAppendOnlyExample} from './cdk-virtual-scroll-append-only-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkVirtualScrollAppendOnlyExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [CdkVirtualScrollAppendOnlyExample],
+})
+export class AppModule {}
